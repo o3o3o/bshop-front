40874ef833c6ee3a71dffa2aa8d464b3ccc9033e
michael <tianya0yingzi@gmail.com>
2019-08-31 17:14:40 +0800

    support login

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,9 +2,7 @@
 	<view>
 		<view class="uni-padding-wrap">
 			<view class="uni-flex uni-column">
-				<view class="uni-flex-item flex-item-V uni-bg-red" @tap="scanQR">
-					<text class="text">扫码付款</text>
-				</view>
+				<view class="uni-flex-item flex-item-V uni-bg-red" @tap="scanQR"><text class="text">扫码付款</text></view>
 				<view class="uni-flex-item flex-item-V uni-bg-green" @tap="deposit"><text class="text">充值</text></view>
 				<view class="uni-flex-item flex-item-V uni-bg-blue" @tap="withdraw"><text class="text">提现</text></view>
 			</view>
@@ -13,98 +11,64 @@
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: '聚赢农产品'
-			}
-		},
-		onLoad() {
-			uni.getProvider({
-				service: 'oauth',
-				success: (result) => {
-					this.providerList = result.provider.map((value) => {
-						let providerName = '';
-						switch (value) {
-							case 'weixin':
-								providerName = '微信登录'
-								break;
-							case 'qq':
-								providerName = 'QQ登录'
-								break;
-							case 'sinaweibo':
-								providerName = '新浪微博登录'
-								break;
-							case 'xiaomi':
-								providerName = '小米登录'
-								break;
-							case 'alipay':
-								providerName = '支付宝登录'
-								break;
-							case 'baidu':
-								providerName = '百度登录'
-								break;
-							case 'toutiao':
-								providerName = '头条登录'
-								break;
-						}
-						return {
-							name: providerName,
-							id: value
-						}
-					});
-			
+import { mapState } from 'vuex';
+export default {
+	data () {
+		return {
+
+		}
+	},
+	computed: {
+		...mapState(['hasLogin'])
+	},
+	onLoad() {
+		if (!this.hasLogin) {
+			uni.reLaunch({
+				url: '../login/login'
+			});
+		}
+	},
+	methods: {
+		scanQR(e) {
+			console.log(e);
+
+			uni.scanCode({
+				//onlyFromCamera: true,
+				success: function(res) {
+					console.log('条码类型：' + res.scanType);
+					console.log('条码内容：' + res.result);
 				},
-				fail: (error) => {
-					console.log('获取登录通道失败', error);
+				fail: function(res) {
+					console.log(res);
 				}
 			});
 		},
-		methods: {
 
-			scanQR(e) {
-				console.log(e);
-				 
-				uni.scanCode({
-					//onlyFromCamera: true,
-					success: function(res) {
-						console.log('条码类型：' + res.scanType);
-						console.log('条码内容：' + res.result);
-					},
-					fail: function(res){
-						console.log(res);
-					},
-				});
-			},
-			
-			deposit(e) {
-				uni.requestPayment({
-					provider: '',
-					orderInfo: '',
-					success: res => {},
-					fail: () => {},
-					complete: () => {}
-				});
-			}
-			
-			
+		deposit(e) {
+			uni.requestPayment({
+				provider: '',
+				orderInfo: '',
+				success: res => {},
+				fail: () => {},
+				complete: () => {}
+			});
 		}
 	}
+};
 </script>
 
 <style>
-	.flex-item-V {
+.flex-item-V {
+	width: 100%;
+	height: 350rpx;
+	line-height: 350rpx;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	align-content: space-around;
+}
 
-		width: 100%;
-		height: 350rpx;
-		line-height: 350rpx;
-		justify-content: center;
-		align-items: center;
-		text-align: center;
-		align-content: space-around;
-	}
-
-	.text {
-		font-size: 50rpx;
-	}
+.text {
+	font-size: 50rpx;
+}
 </style>
---
+++ pages/login/login.vue
@@ -0,0 +1,70 @@
+<template>
+	<view>
+		<block v-if="hasLogin === false"><view class="uni-hello-text uni-center">请点击按钮登录</view></block>
+		<view class="uni-btn-v uni- uni-common-mt">
+			<button type="primary" class="page-body-button" v-for="(value, key) in providerList" @click="tologin(value)" :key="key">{{ value.name }}</button>
+		</view>
+	</view>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+export default {
+	data() {
+		return {
+			providerList: []
+		};
+	},
+	onLoad() {
+		uni.getProvider({
+			service: 'oauth',
+			success: result => {
+				this.providerList = result.provider.map(value => {
+					let providerName = '';
+					switch (value) {
+						case 'weixin':
+							providerName = '微信登录';
+							break;
+						case 'alipay':
+							providerName = '支付宝登录';
+							break;
+					}
+					return {
+						name: providerName,
+						id: value
+					};
+				});
+			},
+			fail: error => {
+				console.log('获取登录通道失败', error);
+			}
+		});
+	},
+	methods: {
+		...mapMutations(['login']),
+		tologin(provider) {
+			uni.login({
+				provider: provider.id,
+				// #ifdef MP-ALIPAY
+				scopes: 'auth_user', //支付宝小程序需设置授权类型
+				// #endif
+				success: res => {
+					console.log('login success:', res);
+					// 更新保存在 store 中的登录状态
+					this.login(provider.id);
+				},
+				fail: err => {
+					console.log('login fail:', err);
+				}
+			});
+		}
+	}
+};
+</script>
+
+<style>
+button {
+	background-color: #007aff;
+	color: #ffffff;
+}
+</style>
