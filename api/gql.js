406af96875067d73045a8ea9cd5c6400a40b425f
michael <tianya0yingzi@gmail.com>
2019-11-06 15:50:41 +0800

    save gqlc in store

--- api/gql.js
+++ api/gql.js
@@ -1,33 +1,12 @@
-// https://github.com/kqtec/graphql-uni-app-client
-import client from "@kqtec/graphql-uni-app-client";
+import store from "@/store";
+
 var { parse } = require("parse-graphql");
 
-var serverUrl;
-
-if (process.env.NODE_ENV === "development") {
-	console.log("开发环境");
-	serverUrl = "http://192.168.1.107:8000/api/gql";
-} else {
-	console.log("生产环境");
-	serverUrl = "https://bshop.o3o3o.com/api/gql";
-}
-
-export var gqlc = new client({
-	uri: serverUrl,
-	//TODO: add headers? or use lokka-transport-jwt-auth?
-	headers: {}
-});
-//console.log(gqlc);
-
-export default function updateJwtToken(token) {
-	//TODO: update and refresh jwt token
-	//https://github.com/kadirahq/lokka-transport-jwt-auth/blob/master/src/index.js
-	gqlc.$client._transport._$headers.Authorization = "JWT " + token;
-	// console.log("update to new token: ", gqlc.$client._transport._$headers);
-}
+console.log("store: ", store);
+console.log(store, store.state.gqlc);
 
 export function execute(query, variables = null, name = null) {
-	return gqlc
+	return store.state.gqlc
 		.query(query, variables)
 		.then(res => {
 			if (name === null) {
@@ -77,29 +56,6 @@ export function mutate_without_result(query, variables, name) {
 			Promise.reject(res.message);
 		}
 	});
-}
-
-export function testGql() {
-	let gqlc = new client({
-		uri: serverUrl
-	});
-	const query = `
-	   query UserQuery {
-	     Movie(title: "Inception") {
-	       releaseDate
-	       actors {
-	         name
-	       }
-	     }
-	     allMovies{
-	       id
-	       slug
-	       title
-	     }
-	   }
-	
-	`;
-	return execute(query);
 }
 
 function parseQueryName(query) {
